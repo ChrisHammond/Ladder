3ab1d3e7d9af48013a20eab25e513e11c5ba903b
Chris Hammond <github@christoc.com>
2012-08-21 05:58:06 +0000

    Converted to VS2012, working on service later, not working currently

--- Module/Edit.ascx
+++
@@ -1,1 +0,0 @@
-<%@ Control language="C#" Inherits="com.christoc.modules.ladder.Edit" AutoEventWireup="false"  Codebehind="Edit.ascx.cs" %>
--- Module/Services/LadderController.cs
+++ Module/Services/LadderController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Christoc.Com.Modules.Ladder.Components;
 using DotNetNuke.Instrumentation;
-using DotNetNuke.Security;
 using DotNetNuke.Web.Services;
+using System.Web.Script.Serialization;
 
 namespace Christoc.Com.Modules.Ladder.Services
 {
@@ -15,13 +12,12 @@ namespace Christoc.Com.Modules.Ladder.Services
 
         //check to make sure we can get/use games
         [DnnAuthorize(AllowAnonymous = true)]
-        public ActionResult GetGames(int tabId, int moduleId)
+        public ActionResult ListOfGames(int tabId, int moduleId)
         {
             try
             {
                 var gc = new GameController();
-
-                var games = gc.GetGames(PortalSettings.PortalId,true);
+                var games = gc.GetGames(PortalSettings.PortalId, true);
                 return Json(games, JsonRequestBehavior.AllowGet);
             }
             catch (Exception exc)
@@ -32,58 +28,72 @@ namespace Christoc.Com.Modules.Ladder.Services
         }
 
         //below code is not currently in use and doesn't yet work
-        [SupportedModules("Ladder")]
-        [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.View)]
-        public ActionResult SaveGame(int tabId, int moduleId, Game currentGame)
+        //[SupportedModules("Ladder")]
+        //[DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.Edit)]
+        [DnnAuthorize(AllowAnonymous = true)]
+        [AcceptVerbs(HttpVerbs.Post)]
+        //TODO: not bringing over the data correctly
+        public ActionResult SaveGame(string test)
         {
-            try
+            var jsonGame = string.Empty;
+            if (jsonGame != null)
             {
-                var gc = new GameController();
-                //TODO: Save Game
-                if (currentGame != null)
+
+                //deserialize the game
+                var s = new JavaScriptSerializer();
+                var currentGame = (Game)s.DeserializeObject(jsonGame);
+
+                try
                 {
-                    //we need to lookup the game based on the GameId if passed in the json, if so, then combine all the new stats
-                    if (currentGame.GameId > 0)
+                    var gc = new GameController();
+                    //TODO: Save Game
+                    if (currentGame != null)
                     {
-                        var existingGame = gc.GetGame(currentGame.GameId);
-                        currentGame.FieldIdentifier = existingGame.FieldIdentifier;
-                        currentGame.CreatedDate = existingGame.CreatedDate;
-                        currentGame.CreatedByUserId = existingGame.CreatedByUserId;
-                    }
-                    else
-                    {
-                        currentGame.CreatedDate = DateTime.Now;
-                        currentGame.CreatedByUserId = 1;
-                        //check if a Field exists, if not add it
-                        var f = FieldController.GetField(currentGame.FieldIdentifier);
-                        if (f == null)
+                        //we need to lookup the game based on the GameId if passed in the json, if so, then combine all the new stats
+                        if (currentGame.GameId > 0)
                         {
-                            f = new Field { FieldIdentifier = currentGame.FieldIdentifier, FieldName = currentGame.FieldIdentifier, CreatedByUserId = 1, CreatedDate = DateTime.Now, LastUpdatedByUserId = 1, LastUpdatedDate = DateTime.Now };
-                            f = f.Save();
+                            var existingGame = gc.GetGame(currentGame.GameId);
+                            currentGame.FieldIdentifier = existingGame.FieldIdentifier;
+                            currentGame.CreatedDate = existingGame.CreatedDate;
+                            currentGame.CreatedByUserId = existingGame.CreatedByUserId;
                         }
-                        currentGame.FieldIdentifier = f.FieldIdentifier;
+                        else
+                        {
+                            currentGame.CreatedDate = DateTime.Now;
+                            currentGame.CreatedByUserId = 1;
+                            //check if a Field exists, if not add it
+                            var f = FieldController.GetField(currentGame.FieldIdentifier);
+                            if (f == null)
+                            {
+                                f = new Field { FieldIdentifier = currentGame.FieldIdentifier, FieldName = currentGame.FieldIdentifier, CreatedByUserId = 1, CreatedDate = DateTime.Now, LastUpdatedByUserId = 1, LastUpdatedDate = DateTime.Now };
+                                f = f.Save();
+                            }
+                            currentGame.FieldIdentifier = f.FieldIdentifier;
+                        }
+
+                        currentGame.LastUpdatedByUserId = 1;
+                        currentGame.PlayedDate = DateTime.Now;
+                        currentGame.PortalId = PortalSettings.PortalId;
+
+                        //save the game information
+                        currentGame = gc.SaveGame(currentGame);
+
+                        //TODO: how to flag games in progress?
+
+                        //return the GameID so Netduino knows to use this
+                        return Json(currentGame.GameId, JsonRequestBehavior.AllowGet);
                     }
 
-                    currentGame.LastUpdatedByUserId = 1;
-                    currentGame.PlayedDate = DateTime.Now;
-                    currentGame.PortalId = PortalSettings.PortalId;
 
-                    //save the game information
-                    currentGame = gc.SaveGame(currentGame);
-
-                    //TODO: how to flag games in progress?
-
-                    //return the GameID so Netduino knows to use this
-                    return Json(currentGame.GameId, JsonRequestBehavior.AllowGet);
+                    return null;
                 }
-                
-                return null;
+                catch (Exception exc)
+                {
+                    DnnLog.Error(exc);
+                    return Json(null, JsonRequestBehavior.AllowGet);
+                }
             }
-            catch (Exception exc)
-            {
-                DnnLog.Error(exc);
-                return Json(null, JsonRequestBehavior.AllowGet);
-            }
+            return null;
         }
 
 
