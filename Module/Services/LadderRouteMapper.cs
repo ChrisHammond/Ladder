f6c356598fbd9a2da87ead89baf6073c4039b75b
christoc_cp <SND\christoc_cp>
2012-08-22 06:35:54 +0000

    Services aren't currently working. Converted project to VS2012

--- Module/Services/LadderRouteMapper.cs
+++ Module/Services/LadderRouteMapper.cs
@@ -1,13 +1,24 @@
-﻿using DotNetNuke.Web.Services;
+﻿using System.Linq;
+using System.Web.Mvc;
+using DotNetNuke.Web.Services;
+using Microsoft.Web.Mvc;
 
 namespace Christoc.Com.Modules.Ladder.services
 {
     public class LadderRouteMapper : IServiceRouteMapper
     {
+        private JsonValueProviderFactory _existingFactory;
+
         public void RegisterRoutes(IMapRoute mapRouteManager)
         {
             mapRouteManager.MapRoute("Ladder", "{controller}.ashx/{action}",
                                      new[] { "Christoc.Com.Modules.Ladder.services" });
+            //Remove and JsonValueProviderFactory and add JsonDotNetValueProviderFactory
+            if (ValueProviderFactories.Factories.OfType<JsonValueProviderFactory>().FirstOrDefault() == null)
+            {
+                ValueProviderFactories.Factories.Add(new JsonValueProviderFactory());
+            }
+
         }
     }
 }
